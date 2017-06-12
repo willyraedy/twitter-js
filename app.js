2437d3b5c1a6c827e6af25367d9e1c2cd93d016d
Willy Raedy <wraedy@gmail.com>
2017-06-12 17:08:18 -0500

    added morgan, scripts to json

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 let express = require('express');
+let morgan = require('morgan');
+
+
 let app = express();
 let port = 3000;
+
+app.use(morgan('dev'));
+
+app.use('/special', function(req, res, next) {
+  console.log('you made it to the special area');
+})
 
 app.get('/', function(req, res, next) {
   res.send('Hi!');
 });
+
+app.get('/news', function(req, res, next) {
+
+  res.send('No news today');
+})
 
 
 
