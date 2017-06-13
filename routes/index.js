899c8454c4cb7cc9f7398bf7de9b27be281f0551
Raj Kadiyala <rskadiy@gmail.com>
2017-06-13 11:21:20 -0500

    did auto fill in form

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
 
+// parse application/x-www-form-urlencoded
+router.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+router.use(bodyParser.json());
+
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
+  res.render( 'index', { tweets: tweets, showForm: true } );
 });
 
 router.get('/users/:name', function(req, res) {
   var name = req.params.name;
+  console.log('Nameeeeeeeeeeeee', name);
   var list = tweetBank.find( {name: name} );
-  res.render( 'index', { tweets: list } );
+  res.render( 'index', { tweets: list, showForm: true, name: name});
+});
+
+router.post('/tweets', function(req, res){
+  let add = tweetBank.add(req.body.name, req.body.text);
+  console.log(req.body.name, req.body.text);
+  res.redirect('/');
+  //console.log(tweetBank.find())
 });
 
 // let options = {
